1a2dbc55fe54479e36cfa5c320edc2a35bceed46
cleinduquez11 <cleinduquez11@gmail.com>
2023-10-02 15:57:33 +0800

    Added Validation

--- server/controllers/docController.js
+++ server/controllers/docController.js
@@ -9,11 +9,24 @@ const secret_key = process.env.SECRET_ACCESS_TOKEN;
 function Add(req, res) {
     const {Name, Description } = req.body;
     const {UNIQUESUFFIX} = req;
-    console.log(UNIQUESUFFIX)
-    res.json({
-        "Name": Name,
-        "Description": Description,
-        "filename": UNIQUESUFFIX});
+    if(!Name || !Description || !UNIQUESUFFIX ){
+        const fields = {
+            name: Name,
+            description: Description,
+            file: UNIQUESUFFIX
+        }
+        res.status(400).json({
+            "Status": 400,
+            "Message": `${!Name? "Name,": ""}${!Description? "Description,":""}${!UNIQUESUFFIX? "File":""} field(s) need to have values `,
+        });
+    }
+    else {
+        res.status(200).json({
+            "Status": 200,
+            "Message": `${UNIQUESUFFIX} Uploaded Successfully`,
+        });
+    }
+    
     
 }
 
